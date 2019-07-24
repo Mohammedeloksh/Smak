7790f42961d42517537fad92f59b434a93b05452
mohammed eloksh <mmaeloksh@gmail.com>
2019-07-24 22:23:51 +0200

    ELOKSH PROJECT

---
+++ Smack2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack2
+//
+//  Created by macbookpro on 7/24/19.
+//  Copyright © 2019 macbookpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
